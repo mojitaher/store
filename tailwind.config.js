65f4c217a5eda68d72ebb1c873d304ee1b59fca0
parmis <parmis.khorshidi@gmail.com>
2025-12-01 00:20:53 +0330

    fix:  config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+// import flowbite from "flowbite-react/tailwind";
 module.exports = {
     content: [
       "./app/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/flowbite/**/*.js" ,
+      flowbite.content(),
     ],
     theme: {
       extend: {},
     },
-    plugins: [],
+    plugins: [
+      require('flowbite/plugin'),
+      flowbite.plugin(),
+    ],
   }
   
